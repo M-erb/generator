20c582fc93fd01a6ea4bc6d3e2ea7dc758a806f5
Michael Erb <Michael Erb>
2016-12-06 16:41:39 -0700

    more styles and btns

--- app/app.js
+++ app/app.js
@@ -5,12 +5,24 @@ skuApp.controller('SKUgenCtrl', ['$scope', '$cookies', '$localStorage', '$sessio
   $scope.storage = $localStorage;
   //$scope.pageClass = 'directory';
 
+  // FOR TESTING remove once done
+  $scope.genPushed = true
+
   //filling data
   $scope.frontLetters = 'SKU'
   $scope.backLetters = ''
   $scope.genCount = 30
   $scope.genStarNum = 0
   $scope.gendigits = 3
+
+  $scope.numCheck = function() {
+    if ($scope.genCount < 1) {
+      $scope.genCount = 1
+    }
+    if ($scope.genStarNum < 0) {
+      $scope.genStarNum = 0
+    }
+  }
 
   $scope.genConfig = {}
   $scope.sku = []
@@ -20,6 +32,7 @@ skuApp.controller('SKUgenCtrl', ['$scope', '$cookies', '$localStorage', '$sessio
     $scope.genConfig.backLetters = $scope.backLetters
     $scope.genConfig.genCount = $scope.genCount
     $scope.genConfig.genStarNum = $scope.genStarNum
+    $scope.genConfig.gendigits = $scope.gendigits
 
     //change button
     $scope.genPushed = true
@@ -35,10 +48,35 @@ skuApp.controller('SKUgenCtrl', ['$scope', '$cookies', '$localStorage', '$sessio
     for(var i=$scope.genConfig.genStarNum; i < $scope.genConfig.genCount + $scope.genConfig.genStarNum; i++) {
       var numFormat = function(n) {
         // adds zeros before the numbers as needed
-        if(n <= 9) {
-          return n > 9 ? "0" + n: "00" + n;
-        }else {
-          return n > 99 ? "" + n: "0" + n;
+        switch ($scope.genConfig.gendigits) {
+          case 1:
+            return n
+            break
+          case 2:
+            if(n <= 9) {
+              return n > 9 ? "" + n: "0" + n
+            }else {
+              return n > 99 ? "" + n: "" + n
+            }
+            break
+          case 3:
+            if(n <= 9) {
+              return n > 9 ? "0" + n: "00" + n
+            }else {
+              return n > 99 ? "" + n: "0" + n
+            }
+            break
+          case 4:
+            if(n <= 9) {
+              return n > 9 ? "00" + n: "000" + n
+            }else if (n <= 99) {
+                return n > 99 ? "0" + n: "00" + n
+            }else if (n <= 999) {
+              return n > 999 ? "" + n: "0" + n
+            }else {
+              return n
+            }
+            break
         }
       }
 
@@ -49,7 +87,6 @@ skuApp.controller('SKUgenCtrl', ['$scope', '$cookies', '$localStorage', '$sessio
   $scope.clearGen = function() {
     $scope.sku = []
     $scope.genPushed = false
-    $scope.genConfig = {}
   }
 
   $scope.clearConfig = function() {
